039cfd4ad0bebe57b44929bf8ec675758df2e3ea
abhiseksaikia <saikia.abhisek@gmail.com>
2023-04-03 23:22:14 -0700

    Track failure propagation time from worker to coordinator

--- presto-main/src/main/java/com/facebook/presto/execution/SqlStageExecution.java
+++ presto-main/src/main/java/com/facebook/presto/execution/SqlStageExecution.java
@@ -608,7 +608,7 @@ public final class SqlStageExecution
                         .orElse(new PrestoException(GENERIC_INTERNAL_ERROR, "A task failed for an unknown reason"));
                 if (isRecoverable(rewrittenFailures)) {
                     try {
-                        stageTaskRecoveryCallback.get().recover(taskId);
+                        stageTaskRecoveryCallback.get().recover(taskId, rewrittenFailures);
                         totalRetries.incrementAndGet();
                         finishedTasks.add(taskId);
                     }
@@ -749,7 +749,8 @@ public final class SqlStageExecution
                 executionFailureInfo.getErrorLocation(),
                 isLeaf ? REMOTE_HOST_GONE.toErrorCode() : REMOTE_HOST_GONE_INTERMEDIATE.toErrorCode(),
                 executionFailureInfo.getRemoteHost(),
-                executionFailureInfo.getErrorCause());
+                executionFailureInfo.getErrorCause(),
+                executionFailureInfo.getFailureDetectionTimeInNanos());
     }
 
     @Override
@@ -815,7 +816,7 @@ public final class SqlStageExecution
     @FunctionalInterface
     public interface StageTaskRecoveryCallback
     {
-        void recover(TaskId taskId);
+        void recover(TaskId taskId, List<ExecutionFailureInfo> executionFailureInfos);
     }
 
     private static class ListenerManager<T>
