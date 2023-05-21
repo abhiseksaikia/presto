45eac2619094db231afbf6bf39b7173b079c9536
Paul Meng <paulmeng@fb.com>
2023-05-21 14:27:40 -0400

    Fetch the data by slurp size

--- presto-main/src/test/java/com/facebook/presto/operator/TestExchangeClient.java
+++ presto-main/src/test/java/com/facebook/presto/operator/TestExchangeClient.java
@@ -553,6 +553,7 @@ public class TestExchangeClient
     private ExchangeClient createExchangeClient(MockExchangeRequestProcessor processor, DataSize bufferCapacity, DataSize maxResponseSize)
     {
         return new ExchangeClient(
+                new DataSize(204, MEGABYTE),
                 bufferCapacity,
                 maxResponseSize,
                 1,
